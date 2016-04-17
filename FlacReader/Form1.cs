bab588b146bd0d88eecf486b02abfe4c9d577cee
Alberto Zichittella <a.zichittella@gmail.com>
2016-04-17 22:09:16 +0200

    Aggiunta interfaccia grafica

--- FlacReader/Form1.cs
+++ FlacReader/Form1.cs
@@ -8,13 +8,44 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace FlacReader
+namespace it.albe
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonFile_Click(object sender, EventArgs e)
+        {
+            FileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+                textBoxFile.Text = dialog.FileName;
+            caricaFile();
+        }
+        private void caricaFile()
+        {
+            if (textBoxFile.Text == "")
+                return;
+            FlacReader flacReader = new FlacReader(textBoxFile.Text);
+            DataTable data = new DataTable();
+            data.Columns.Add("comment");
+            data.Columns.Add("value");
+            foreach (KeyValuePair<string, string> entry in flacReader.comments)
+            {
+                DataRow row = data.NewRow();
+                row[0] = entry.Key;
+                row[1] = entry.Value;
+                data.Rows.Add(row);
+            }
+
+            dataGridView1.DataSource = data;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
--- FlacReader/Program.cs
+++ FlacReader/Program.cs
@@ -4,23 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-
+using System.Windows.Forms;
 namespace it.albe
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            FlacReader flacReader = new FlacReader("Hallelujah.flac");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
             /*Console.WriteLine(flacReader.getVendor());
             Console.WriteLine(flacReader.comments["TITLE"]);
             Console.WriteLine(flacReader.comments["ALBUM"]);
             Console.ReadLine();
-            */
+            
             flacReader.setVendor("ciao mi chiamo alberto sto creando questo programma che e un flacreader");
             flacReader.addComment("ARTIST", "Bruce Springsteen");
             flacReader.addComment("ALBUM", "CIANOBATTERIO");
-            flacReader.writeAll();
+            flacReader.writeAll();*/
         }
     }
 }
